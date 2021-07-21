85f41d17ca613a893b6fde7d3ea55b47b3573f38
lucasdemicco <lucasmicco@hotmail.com>
2021-07-21 13:35:48 -0300

    Controller dos formulários

--- pages/login_page.dart
+++ pages/login_page.dart
@@ -2,7 +2,9 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({Key? key}) : super(key: key);
+
+  final _tLogin = TextEditingController();
+  final _tSenha = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -19,15 +21,15 @@ class LoginPage extends StatelessWidget {
       padding: EdgeInsets.all(16),
       child: ListView(
         children: <Widget>[
-          _textFormField(),
+          _textFormField("Login", "Digite seu login", controller: _tLogin),
           SizedBox(
             height: 10,
           ),
-          _textFormFieldSenha(),
+          _textFormField("Senha", "Digite sua senha", password: true, controller: _tSenha),
           SizedBox(
             height: 20,
           ),
-          _container()
+          _button("Login", _onClickLogin)
         ],
       ),
     );
@@ -35,41 +37,42 @@ class LoginPage extends StatelessWidget {
 
   //MÉTODOS WIDGETS
 
-  Container _container() {
-    return Container(
-      height: 46,
-      child: RaisedButton(
-        color: Colors.blue,
-        onPressed: () {},
-        child: Text(
-          "Login",
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 22,
-          ),
-        ),
-      ),
-    );
-  }
-
-  TextFormField _textFormField() {
+   _textFormField(String label, String hint, {bool password = false, controller}) {
     return TextFormField(
+      controller: controller,
+      obscureText: password,
       style: TextStyle(color: Colors.blue, fontSize: 20),
       decoration: InputDecoration(
-          labelText: "Login",
+          labelText: label,
           labelStyle: TextStyle(color: Colors.grey, fontSize: 25),
-          hintText: "Digite o seu login"),
+          hintText: hint),
     );
+  }
+
+ _button(String text, onPressed) {
+  return Container(
+    height: 46,
+    child: RaisedButton(
+      color: Colors.blue,
+      child: Text(
+        text,
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 22,
+        ),
+      ),
+      onPressed: onPressed,
+    ),
+  );
+}
+
+  _onClickLogin() {
+    String login = _tLogin.text;
+    String senha = _tSenha.text;
+
+    print("Login: $login, Senha:$senha");
   }
 }
 
-TextFormField _textFormFieldSenha() {
-  return TextFormField(
-    style: TextStyle(color: Colors.blue, fontSize: 20),
-    obscureText: true,
-    decoration: InputDecoration(
-        labelText: "Senha",
-        labelStyle: TextStyle(color: Colors.grey, fontSize: 25),
-        hintText: "Digite a sua senha"),
-  );
-}
+
+
